7bb7baa9b6c83faa34281d56641fc2adff532f10
Vento <vento.michael0705@hotmail.com>
2020-11-13 20:29:31 +0100

    try to display success message

--- bookApp/public/js/app.js
+++ bookApp/public/js/app.js
@@ -1,10 +1,21 @@
 function stayOpenFunction() {
     let formSearch = document.getElementById("formSearch");
-    formSearch.addEventListener('input',()=>{
-        if (formSearch.value !== ''){
-            formSearch.classList.add('w-48','py-1','px-3')
-        }else {
-            formSearch.classList.remove('w-48','py-1','px-3')
+    formSearch.addEventListener('input', () => {
+        if (formSearch.value !== '') {
+            formSearch.classList.add('w-48', 'py-1', 'px-3')
+        } else {
+            formSearch.classList.remove('w-48', 'py-1', 'px-3')
         }
     })
 }
+
+    setTimeOut(function () {
+        document.getElementById("sucessMessage").remove()
+    }, 3000);
+
+setInterval(function(){
+
+    var obj = document.getElementById("sucessMessage");
+    obj.innerHTML = "dezdezdze";
+
+},3000);
